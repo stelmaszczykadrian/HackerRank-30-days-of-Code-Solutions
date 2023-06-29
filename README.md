b8943c421e4ba395de098014b3943c2138aa6d70
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-06-29 15:37:39 +0200

    Initial repo

--- README.md
+++
@@ -1,1 +0,0 @@
-# HackerRank-30-days-of-Code-Solutions
---
+++ src/test/java/hackerRank/solutions/SolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package hackerRank.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
