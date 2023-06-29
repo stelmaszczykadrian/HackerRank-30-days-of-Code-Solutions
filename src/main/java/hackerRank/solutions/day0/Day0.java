ab8ac7a6b0fb93e6f3de4f11052d1f6a5b349621
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-06-29 15:44:49 +0200

    Day 0 done.

---
+++ src/main/java/hackerRank/solutions/day0/Day0.java
@@ -0,0 +1,14 @@
+package hackerRank.solutions.day0;
+
+import java.util.Scanner;
+
+public class Day0 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+
+        System.out.println("Hello, World.");
+        System.out.println(name);
+    }
+}
