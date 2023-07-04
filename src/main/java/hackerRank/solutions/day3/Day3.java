b56b409f76b52c3e44d2b6d7402cd5475104adff
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-07-04 12:41:58 +0200

    Day 3 done.

---
+++ src/main/java/hackerRank/solutions/day3/Day3.java
@@ -0,0 +1,28 @@
+package hackerRank.solutions.day3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day3 {
+    public class Solution {
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+            bufferedReader.close();
+
+            if (isEven(n) && (n > 20 || (n >= 2 && n <= 5))) {
+                System.out.println("Not Weird");
+            } else {
+                System.out.println("Weird");
+            }
+        }
+
+        private static boolean isEven(int n) {
+            return n % 2 == 0;
+        }
+    }
+
+}
